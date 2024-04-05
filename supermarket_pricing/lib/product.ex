3c2d4603022b15ea85d7ed618042aef5fdb4aabb
gunkim <gunkim.dev@gmail.com>
2024-04-06 00:01:38 +0900

    Add documentation and type specifications to product modules

--- supermarket_pricing/lib/product.ex
+++ supermarket_pricing/lib/product.ex
@@ -1,26 +1,50 @@
 defmodule ProductHelper do
+  @moduledoc """
+    Module for the ProductHelper struct to model products.
+  """
+
   @doc """
     Converts a price to a dollar format.
+
+    ## Example
+        iex> ProductHelper.to_dollar(Decimal.new("1.5"))
+        "$1.5"
   """
+  @spec to_dollar(Decimal.t) :: String.t
   def to_dollar(price) do
     "$#{Decimal.to_string(price)}"
   end
 end
 
 defprotocol Product do
+  @moduledoc """
+    Protocol for the Product struct to model products.
+  """
+
   @fallback_to_any true
 
   @doc """
     Prints the product information.
   """
+  @spec print_product(Product.t) :: :ok
   def print_product(product)
+
   @doc """
     Calculates the price of the product.
   """
+  @spec price(Product.t, Decimal.t) :: Decimal.t
   def price(product, amount)
 end
 
 defmodule SingleProduct do
+  @moduledoc """
+    Module for the SingleProduct struct to model products.
+
+    # Example
+        iex> %SingleProduct{name: "Peanut Can", price: Decimal.new("0.65")}
+        %SingleProduct{name: "Peanut Can", price: Decimal.new("0.65")}
+  """
+
   defstruct [:name, :price]
 
   defimpl Product do
@@ -33,6 +57,14 @@ defmodule SingleProduct do
 end
 
 defmodule BundleProduct do
+  @moduledoc """
+    Module for the BundleProduct struct to model products.
+
+    # Example
+        iex> %BundleProduct{name: "Apple", price: Decimal.new("1"), bundle_size: 3}
+        %BundleProduct{name: "Apple", price: Decimal.new("1"), bundle_size: 3}
+  """
+
   defstruct [:name, :price, :bundle_size]
 
   defimpl Product do
@@ -49,7 +81,16 @@ defmodule BundleProduct do
 end
 
 defmodule FoundProduct do
+  @moduledoc """
+    Module for the FoundProduct struct to model products.
+
+    # Example
+        iex> %FoundProduct{name: "Banana", price: Decimal.new("1.99"), found: 4}
+        %FoundProduct{name: "Banana", price: Decimal.new("1.99"), found: 4}
+  """
+
   defstruct [:name, :price, :found]
+
   defimpl Product do
     def print_product(product) do
       IO.puts "Product Name: #{product.name}, Price: #{product.found} pounds for #{ProductHelper.to_dollar(product.price)} per unit"
