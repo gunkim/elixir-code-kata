be2dfea2bfaba43ed88de30e59e5b2d0e6c0ec17
gunkim <gunkim.dev@gmail.com>
2024-04-08 23:34:24 +0900

    Add ProductsTest module with case sensitivity tests

---
+++ supermarket_pricing/test/products_test.exs
@@ -0,0 +1,28 @@
+defmodule ProductsTest do
+  use ExUnit.Case
+  doctest Products
+
+  setup do
+    products = [
+      %SingleProduct{name: "Peanut Can", price: Decimal.new("0.65")},
+      %BundleProduct{name: "Apple", price: Decimal.new("1"), bundle_size: 3},
+      %FoundProduct{name: "Banana", price: Decimal.new("1.99"), found: 4},
+      %SingleProduct{name: "Shampoo", price: Decimal.new("1.5")}
+    ]
+    expectedProduct = %BundleProduct{name: "Apple", price: Decimal.new("1"), bundle_size: 3}
+
+    {:ok, %{products: products, expectedProduct: expectedProduct}}
+  end
+
+  test "Returns a product when the name is in lowercase", %{products: products, expectedProduct: expectedProduct} do
+    assert Products.search(products, "apple") == expectedProduct
+  end
+
+  test "Returns a product when the name is in uppercase", %{products: products, expectedProduct: expectedProduct} do
+    assert Products.search(products, "APPLE") == expectedProduct
+  end
+
+  test "Returns a product when the name is in mixed case", %{products: products, expectedProduct: expectedProduct} do
+    assert Products.search(products, "AppLe") == expectedProduct
+  end
+end
